67e08289e94591ee853389b14489562558b5aa52
zaaykomomo <bhagawatiguesthouse+second@gmail.com>
2025-07-13 11:56:58 +0530

    Create dark-mode.js
    added

---
+++ dark-mode.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById('darkToggle');
+  const body = document.body;
+
+  if (!toggle) return;
+
+  if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+    toggle.checked = true;
+  }
+
+  toggle.addEventListener('change', () => {
+    if (toggle.checked) {
+      body.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      body.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light');
+    }
+  });
+});
